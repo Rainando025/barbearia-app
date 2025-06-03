e002c75d52fd1e64ae737319e7e416940b151a42
Rainando025 <rainando123@gmail.com>
2025-06-03 08:36:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
-from flask_sqlalchemy import SQLAlchemy
+
 from sqlalchemy import or_
 from datetime import datetime, date, timedelta  # <--- adicionei date aqui
 from werkzeug.security import generate_password_hash, check_password_hash
